3070d2d02b60f6c3cf364512dd5bde7907c5e487
Noisk8 <juansk8jaramillo@gmail.com>
2024-09-11 21:34:55 -0500

    🛸

---
+++ FRONTEND-I/script.js
@@ -0,0 +1,138 @@
+// let variable = 'test';
+
+// let fumela = 'test 2';
+
+// const PI  = 3.14;
+
+
+// console.log(fumela)
+
+
+
+// let persona ={
+
+//     nombre: 'juan',
+//     edad: 30 
+// }
+
+
+// function saludar (){
+
+//     console.log('Hola')
+// }
+
+// saludar();
+
+
+
+
+
+// let hora = 24;
+// let minutos = 30;
+
+// if (hora < 12){
+//     console.log("Buenos días");
+// } else {
+//     console.log("buenas tardes");
+// }
+
+
+// if (hora < 12){
+//     console.log("buena marade")
+//     if(minutos <= 30){
+//         console.log("mamalo elon musk")
+//     }
+// }
+
+
+// let edad = 18 
+
+// if( edad >= 18){
+//     console.log("eres mayor de edad")
+
+// }else{
+//     console.log("mamalo chinga")
+// }
+
+
+// let calificacion = [9,12,34,15,17,56]
+
+
+//  console.log(calificacion)
+
+// //  if (NotaFinal >= 17){
+// //     console.log("sobresaliente")
+// //  }
+
+//  for (let i = 0; i  < calificacion.length; i++){
+
+//    let  NotaFinal = [i];
+//    console.log(NotaFinal)
+
+
+//  }
+
+
+
+
+// switch case 
+
+
+// let dia = 9;
+
+// switch(dia ){
+//     case 1:
+//         console.log("Lunes");
+//         break;
+    
+//         case  2:
+//             console.log("Martes");
+//             break;
+
+//             case 3:
+//                 console.log("Miercoles");
+
+//                 default:
+//                     console.log("PAILAS")
+// }
+
+
+
+// let contador  =0 ;
+// while (contador <  10){
+//     console.log(contador);
+//     contador++;
+// }
+
+// let estudiantes = ["Juan", "Maria", "Pedro", "Juan", "Ana", "Pedro", "Luis"];
+// let nombresRepetidos = [];
+
+
+
+// let numeros = [1, 20, 45, 4, 5];
+
+// let suma = 0;
+
+// for (let i = 0; i < numeros.length; i++) {
+ 
+ 
+//     suma = suma + numeros[i];
+// }
+
+// console.log("La suma de los elementos es: " + suma);
+
+
+
+// suma en while 
+
+let numeros = [1, 20, 45, 4, 5];
+let suma = 0;
+let i = 0;
+
+while (i < numeros.length) {
+    
+    suma = suma + numeros[i];
+  i++;
+}
+
+console.log("la suma de los numeros es: " + suma);
