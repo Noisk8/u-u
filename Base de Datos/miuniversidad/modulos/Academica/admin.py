58837b5bd9abe0deb2ed9bdf6e6391b3b2e2f41b
Noisk8 <juansk8jaramillo@gmail.com>
2024-09-23 21:12:13 -0500

    app django

---
+++ Base de Datos/miuniversidad/modulos/Academica/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from modulos.Academica.models import *
+
+# Register your models here.
+
+admin.site.register(carreras)
+admin.site.register(cursos)
+admin.site.register(estudiantes)
+admin.site.register(matriculas)
